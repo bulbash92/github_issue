d98bd58b6ba3f07cd0a7538bc9e8b82e33a041e6
Evgeniy <y.danchanka@tapston.com>
2023-02-14 23:59:49 +0300

    refactor structure

---
+++ src/Pages/Main.vue
@@ -0,0 +1,24 @@
+<template>
+<h1 class="title"> Тестовое задание для компании Открытый контакт </h1>
+<div class="descriptions">!!для того чтобы github api работал, добавьте свой гитхаб токен в api.js конфиг!!</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+
+.descriptions {
+  text-align: center;
+  font-size: 18;
+  font-weight: bold;
+  color: red;
+  text-transform: uppercase;
+  margin-top: 20px;
+}
+</style>
